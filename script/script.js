2fdb0a95c36bd4b1b8998a2da61a7e87d54b21d0
Archfey Network <fifer@Archfeys-MacBook-Air.local>
2018-12-14 21:47:29 -0500

    make some stuff fade in (ooh nice)

--- script/script.js
+++ script/script.js
@@ -2,4 +2,39 @@ $(document).ready(function() {
   $(".hiddenlist").click(function () {
     $("ul", $(this).parent()).slideToggle();
   });
+
+  $(".cover-text").find("h1").addClass("fade-in");
+  $(".box-wrapper").addClass("fade-in");
+  var fade_time_slow = 2000;
+  var fade_time_fast = 1000;
+
+  $(".fade-in").each(function(i) {
+    var this_bottom  = $(this).offset().top + $(this).outerHeight();
+    var window_bottom = $(window).scrollTop() + $(window).height();
+    var fade_time = fade_time_fast;
+
+    if ($(this).parents(".cover-text") && $(this).is("h1")) {
+      fade_time = fade_time_slow;
+    }
+
+    if (window_bottom > this_bottom) {
+      $(this).animate({'opacity':'1'}, fade_time);
+    }
+  });
+
+  $(window).scroll(function() {
+    $(".fade-in").each(function(i) {
+      var this_bottom = $(this).offset().top + $(this).outerHeight();
+      var window_bottom = $(window).scrollTop() + $(window).height();
+      var fade_time = fade_time_fast;
+
+      if ($(this).parents(".cover-text") && $(this).is("h1")) {
+        fade_time = fade_time_slow;
+      }
+
+      if (window_bottom > this_bottom) {
+        $(this).animate({'opacity':'1'}, fade_time);
+      }
+    });
+  });
 });
